634f8c168b1c50446bec145b943b6731b2e00bf0
somename <someemail>
2015-10-09 13:39:21 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Cell.java
@@ -28,8 +28,8 @@ public class Cell {
 			state = "Dead";
 		else if (state == "Alive" && numberOfLiveNeighbours > 3)
 			state = "Dead";
-		else if (state == "Dead" && numberOfLiveNeighbours == 3)
-			state = "Alive";
+		else if (state == "Dead" && numberOfLiveNeighbours < 3)
+			state = "Dead";
 		else
 			state = "Alive";
 	}
