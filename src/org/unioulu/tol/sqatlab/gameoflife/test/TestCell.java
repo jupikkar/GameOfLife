9da4f317f7f56e1ceed60673d692b0f08eae2fe2
somename <someemail>
2015-10-09 13:39:53 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/test/TestCell.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/test/TestCell.java
@@ -67,5 +67,15 @@ public class TestCell {
 		//assert
 		assertEquals("Dead", cell.getState());
 	}
+	
+	@Test
+	public void testDeadCellWithOverThreeNeighboursShouldStayDead() {
+		//arrange
+		Cell cell = new Cell("Dead");
+		//act
+		cell.nextIteration(4);
+		//assert
+		assertEquals("Dead", cell.getState());
+	}
 
 }
