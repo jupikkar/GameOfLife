546a80da8944bca00af65bf5160498425db9d560
somename <someemail>
2015-10-09 12:56:08 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
+++ src/org/unioulu/tol/sqatlab/gameoflife/Grid.java
@@ -1,6 +1,10 @@
 package org.unioulu.tol.sqatlab.gameoflife;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Grid {
+	private Set<Cell> cells = new HashSet<>();
 
 	public int getNeighbours(Cell cell) {
 		return 0;
